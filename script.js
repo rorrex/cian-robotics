a97a826f06f93cf72825ffbfc50f873c5875be36
Javier Robles <112677654+EllJarvis@users.noreply.github.com>
2025-08-17 20:36:54 -0600

    Base HTML de los porductos individuales

--- script.js
+++
@@ -1,6 +0,0 @@
-const botones = document.querySelectorAll(".btn-mas");
-botones.forEach(boton => {
-  boton.addEventListener("click", () => {
-    alert("Pronto tendrás más información sobre este producto.");
-  });
-});
